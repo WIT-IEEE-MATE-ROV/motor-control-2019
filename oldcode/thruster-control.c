4925131af68ae3cde7b252b104cb84194f0af65e
Chris Thierauf <chris@cthierauf.com>
2019-04-11 19:12:22 -0400

    Cleaned up a bit

--- oldcode/thruster-control.c
+++
@@ -1,9 +0,0 @@
-// This file exists until the actual thruster-control.c can compile.
-#include <unistd.h>
-
-int main(void) {
-    while(1) {
-        // Here's where we would do thruster stuff
-        sleep(1);
-    }
-}
